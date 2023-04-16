eed2a18dc9b21f6e9d32c0a773af740a469698af
QiaoLei532 <1009535143@qq.com>
2023-04-16 16:15:56 +0800

    第一次commit

---
+++ 第2章/2.1-2.py
@@ -0,0 +1,16 @@
+"""
+需求：
+2-1：简单消息：将一条消息存储到变量中，再打印
+2-2：多条简单消息：将一条消息存储变量中，打印；修改变量值，再打印
+"""
+"""
+步骤分析
+1、准备变量
+2、打印
+"""
+message = 'Hello World!'
+print(f'打印message的内容：{message}')
+message = 'Hello Python!'
+print(f'打印修改后message的内容：{message}')
+
+
