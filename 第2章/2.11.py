a7c107f8acc31a3c5e8b44209ed9b1bc21b61aef
QiaoLei532 <1009535143@qq.com>
2023-04-16 17:26:09 +0800

    更新2.8-2.11

---
+++ 第2章/2.11.py
@@ -0,0 +1,5 @@
+"""
+python 之 禅 （原则）
+
+console中输入：import this
+"""
---
+++ 第2章/2.9.py
@@ -0,0 +1,11 @@
+"""
+需求：
+最喜欢的数字
+步骤分析：
+1、准备变量存储数字
+2、拼接打印
+"""
+love_num = '6666'
+message = 'My favourite number is '+love_num
+
+print(message)
