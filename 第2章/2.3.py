4ae5548be36f914a94427f01098a71df4a872384
QiaoLei532 <1009535143@qq.com>
2023-04-16 16:42:54 +0800

    第二次练习，

---
+++ 第2章/2.3.py
@@ -0,0 +1,11 @@
+"""
+需求：
+个性化消息
+步骤分析：
+1、准备一个存储姓名的变量
+2、打印欢迎语句
+"""
+name = 'Lilei'
+print(f'Hello {name},would you like to learn some Python today?')
+print('Hello', name, ',would you like to learn some Python today?') #默认前后空格
+print('Hello'+name+',would you like to learn some Python today?') #字符串拼接，无空格
---
+++ 第2章/2.4.py
@@ -0,0 +1,14 @@
+"""
+需求：
+调整姓名大小写
+步骤分析：
+1、准备一个姓名变量
+2、小写输出
+3、大写输出
+4、首字母大写输出
+"""
+name = 'Han Meimei'
+print('姓名:', name)
+print('全小写：'+name.lower())
+print('全大写：'+name.upper())
+print('首字母大写：'+name.title())
