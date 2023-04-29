f0171d3391bc9d05a2b386ece4444000ec08470f
QiaoLei532 <1009535143@qq.com>
2023-04-29 20:38:45 +0800

    更新3.7-3.10

--- 第3章/3.4-3.7.py
+++ 第3章/3.4-3.7.py
@@ -18,3 +18,20 @@ names.append('xiao hong')
 for i in names:
     print(i+'，你好，我邀请你共进晚餐！')
 #3.6添加嘉宾
+print('找到一个大桌子，加人！')
+names.insert(0, 'Wangshuo')
+names.insert(len(names)//2, 'Tina')
+names.append('Qiaol')
+for i in names:
+    print(i+'，你好，我邀请你共进晚餐！')
+#3.7缩减名单
+print('桌子到不了了，只能邀请2个人！')
+while len(names) > 2:
+    name = names.pop()
+    print(f'抱歉{name},无法邀请你了，下次吧！')
+for i in names:
+    print(i+'，你好，我邀请你共进晚餐！')
+#del删除
+while len(names) > 0:
+    del names[len(names)-1]
+print(names)
